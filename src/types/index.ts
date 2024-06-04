c9e385e0143dae0eadafb6d45425c8bede9e8dd2
Angelina Sokolova <lina.sokolova22@mail.ru>
2024-06-04 10:26:56 +0300

    готово

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,46 +1,84 @@
-//Интерфейс API-клиента: 
-
-export interface IApiClient {
-}
-
-//Интерфейсы модели данных: Эти интерфейсы определяют структуру данных, которые используются в вашем приложении. 
+//Данные карточки:
 
 export interface Product {
     id: number;
     name: string;
     description: string;
     price: number | null;
+    link:string;
+    title: string;
     category: string;
 }
 
-//Интерфейсы отображений: Эти интерфейсы описывают, как данные должны быть представлены на экране. Например, интерфейс для представления товара в каталоге может выглядеть так:
+//Данные пользователя, используемые для оформления заказа:
 
-export interface ProductView {
-    id: number;
-    name: string;
+export interface IUser {
+    paymentType: Pay;
+    adress:string;
+    email:string;
+    phone:string;
+    total: number;
+    items: string;
+    validateUserPayAdress (data:IUser):boolean;
+    validateUserContacts (data:IUser):boolean
+}
+
+//Тип оплаты, который может быть выбран пользователем при оформлении заказа:
+
+type Pay = 'Онлайн'| 'Наличные'
+
+//Интерфейс определяет структуру данных списка карточек товаров:
+
+export interface ICardList  {
+    total:number;
+    cards: Product[];
+    preview: string | null;
+    addBasket(cardId: string, payload: Function | null):void;
+}
+
+//Структура корзины пользователя, включая список товаров и общую стоимость:
+
+export interface IBacket extends Product, IUser {
+    title: string;
     price: number;
-    category: string;
-    imageUrl: string;
+    total: number;
+    deleteBasketCard(card: Product, payload: Function | null): void;
+    changeTotal(total: number, payload: Function | null):void;
 }
 
-//Интерфейсы базовых классов: Эти интерфейсы определяют общие методы и свойства, которые будут использоваться в вашем приложении. Например, интерфейс для управления состоянием корзины может выглядеть так:
 
-export interface CartState {
-    items: Product[];
-    totalPrice: number;
-}
+//Данные, необходимые для превью на главной странице:
 
-//Перечисление событий и их интерфейсы: Если вы используете брокер событий, вам потребуется определить перечисление событий и интерфейсы для каждого события.
+export type PreviewCard = Pick <Product, "category"|"name"| "link"|"price" >
 
-// Перечисление событий
-export enum EventType {
-    PRODUCT_ADDED_TO_CART = 'cart: add',
-    ORDER_PLACED = 'order: place',
-    // Другие события
-}
-  
-  // Интерфейс для события добавления товара в корзину
-export interface ProductAddedToCartEvent {
-    type: EventType.PRODUCT_ADDED_TO_CART;
-    product: Product;
+//Тип карточки товара:
+
+export type CardInfo = Pick<Product, 'category' | 'title' | 'link' | 'price'>;
+
+//Тип карточки товара в модалном окне:
+
+export type CardModalInfo = Pick<Product, 'link' | 'category' | 'title' | 'description' | 'price'>;
+
+//Тип данных в модалке корзины:
+
+export type BasketInfo = Pick<IBacket, 'title' | 'price' | 'title'>;
+
+//Тип данных в модальном окне оформления заказа (почта и телефон):
+
+export type ModalOrderData = Pick<IUser, 'email' | 'phone'>;
+
+//Тип данных в модальном окне оформления заказа(способ оплаты и адрес):
+
+export type ModalOrderPayment = Pick<IUser, 'paymentType' | 'adress'>;
+
+//Тип данных в модальном окне после оформления заказа:
+
+export type ModalВecorated = Pick<IUser, 'total'>
+
+export type ApiPostMethods = 'POST' | 'PUT' | 'DELETE' | 'PATCH';
+
+export interface IApiClient {
+    baseUrl: string;
+    get<T>(uri: string): Promise<T>;
+    post<T>(uri: string, data: object, method?: ApiPostMethods): Promise<T>;
 }
